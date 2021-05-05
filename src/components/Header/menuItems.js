a061690938cb9e1ccb980b5f0f708615fbb2dc51
apple <apple@apples-MacBook-Pro.local>
2021-05-05 20:26:14 +0300

    first commit

---
+++ src/components/Header/menuItems.js
@@ -0,0 +1,183 @@
+export const menuItems = [
+  {
+    name: "",
+    label: "Home",
+    // items: [
+    //   { name: "", label: "Home 1" },
+    //   { name: "landing-2", label: "Home 2" },
+    //   { name: "landing-3", label: "Home 3" },
+    // ],
+  },
+  {
+    name: "pages",
+    label: "Jobs",
+    items: [
+      {
+        name: "search-list",
+        label: "Latest Jobs",
+        // items: [
+        //   { name: "search-grid", label: "Job Grid" },
+        //   { name: "search-list", label: "Job List" },
+        //   { name: "job-details", label: "Job Details" },
+        // ],
+      },
+      {
+        name: "search-grid",
+        label: "Jobs By Category",
+      },
+      // {
+      //   name: "dashboard",
+      //   label: "Dashboard",
+      //   items: [
+      //     { name: "dashboard-main", label: "Dashboard Main" },
+      //     { name: "dashboard-settings", label: "Dashboard Settings" },
+      //     {
+      //       name: "dashboard-applicants",
+      //       label: "Dashboard Applicants",
+      //     },
+      //     { name: "dashboard-jobs", label: "Dashboard Posted Jobs" },
+      //   ],
+      // },
+      // {
+      //   name: "candidate",
+      //   label: "Candidate Pages",
+      //   items: [
+      //     { name: "candidate-profile", label: "Candidate Profile 01" },
+      //     { name: "candidate-profile-2", label: "Candidate Profile 02" },
+      //   ],
+      // },
+      // {
+      //   name: "search",
+      //   label: "Search Pages",
+      //   items: [
+      //     { name: "search-grid", label: "Search Grid" },
+      //     { name: "search-list", label: "Search List 01" },
+      //     { name: "search-list-2", label: "Search List 02" },
+      //   ],
+      // },
+      // {
+      //   name: "company-profile",
+      //   label: "Company Profile",
+      // },
+
+      // {
+      //   name: "essential",
+      //   label: "Essential",
+      //   items: [
+      //     { name: "faq", label: "FAQ" },
+      //     { name: "404", label: "404" },
+      //     { name: "pricing", label: "Pricing" },
+      //     { name: "contact", label: "Contact" },
+      //   ],
+      // },
+      // {
+      //   name: "contact",
+      //   label: "Contact",
+      // },
+    ],
+  },
+  {
+    name: "pages",
+    label: "Company",
+    items: [
+      {
+        name: "search-list",
+        label: "Register",
+        // items: [
+        //   { name: "search-grid", label: "Job Grid" },
+        //   { name: "search-list", label: "Job List" },
+        //   { name: "job-details", label: "Job Details" },
+        // ],
+      },
+      {
+        name: "search-grid",
+        label: "Company List",
+      },
+    ],
+  },
+  {
+    name: "contact",
+    label: "Contact",
+  },
+  {
+    name: "#",
+    label: "Support",
+    isExternal: true,
+  },
+];
+// export const menuItems = [
+//   {
+//     name: "home",
+//     label: "Home",
+//     items: [
+//       { name: "", label: "Home 1" },
+//       { name: "landing-2", label: "Home 2" },
+//       { name: "landing-3", label: "Home 3" },
+//     ],
+//   },
+//   {
+//     name: "pages",
+//     label: "Pages",
+//     items: [
+//       {
+//         name: "job-pages",
+//         label: "Job Pages",
+//         items: [
+//           { name: "search-grid", label: "Job Grid" },
+//           { name: "search-list", label: "Job List" },
+//           { name: "job-details", label: "Job Details" },
+//         ],
+//       },
+//       {
+//         name: "dashboard",
+//         label: "Dashboard",
+//         items: [
+//           { name: "dashboard-main", label: "Dashboard Main" },
+//           { name: "dashboard-settings", label: "Dashboard Settings" },
+//           {
+//             name: "dashboard-applicants",
+//             label: "Dashboard Applicants",
+//           },
+//           { name: "dashboard-jobs", label: "Dashboard Posted Jobs" },
+//         ],
+//       },
+//       {
+//         name: "candidate",
+//         label: "Candidate Pages",
+//         items: [
+//           { name: "candidate-profile", label: "Candidate Profile 01" },
+//           { name: "candidate-profile-2", label: "Candidate Profile 02" },
+//         ],
+//       },
+//       {
+//         name: "search",
+//         label: "Search Pages",
+//         items: [
+//           { name: "search-grid", label: "Search Grid" },
+//           { name: "search-list", label: "Search List 01" },
+//           { name: "search-list-2", label: "Search List 02" },
+//         ],
+//       },
+//       {
+//         name: "company-profile",
+//         label: "Company Profile",
+//       },
+
+//       {
+//         name: "essential",
+//         label: "Essential",
+//         items: [
+//           { name: "faq", label: "FAQ" },
+//           { name: "404", label: "404" },
+//           { name: "pricing", label: "Pricing" },
+//           { name: "contact", label: "Contact" },
+//         ],
+//       },
+//     ],
+//   },
+//   {
+//     name: "https://uxtheme.net/product-support/",
+//     label: "Support",
+//     isExternal: true,
+//   },
+// ];
---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Link from "next/link";
+
+import imgL1Logo from "../../assets/image/ABH-Partners.png";
+import imgL1LogoWhite from "../../assets/image/ABH-Partners.png";
+// import imgL1Logo from "../../assets/image/logo-main-black.png";
+// import imgL1LogoWhite from "../../assets/image/logo-main-white.png";
+
+const Logo = ({ white, height, className = "", ...rest }) => {
+  return (
+    <Link href="/">
+      <a className={`d-block ${className}`} {...rest}>
+        {white ? (
+          <img src={imgL1LogoWhite} alt="" />
+        ) : (
+          <img src={imgL1Logo} alt="" />
+        )}
+      </a>
+    </Link>
+  );
+};
+
+export default Logo;
